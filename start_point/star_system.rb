78fc8c6f336d3110b5ad9f8385ff521e50908656
user <user@users-MacBook-Pro.local>
2019-09-19 13:47:16 +0100

    initial commit

---
+++ start_point/star_system.rb
@@ -0,0 +1,17 @@
+class StarSystem
+  attr_reader :name, :planets
+
+  def initialize(name, planets)
+    @name = name
+    @planets = planets
+  end
+
+  def has_name
+    return @solar_system
+  end
+
+  # def has_planets
+  #
+  # end
+
+end
